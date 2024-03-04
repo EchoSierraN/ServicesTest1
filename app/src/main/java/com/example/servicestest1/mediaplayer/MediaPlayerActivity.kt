3942397c1bd2b101d9e822272882945043784c52
Elias Shemsu <echosierran@gmail.com>
2024-03-04 11:11:19 +0300

    media player package

--- app/src/main/java/com/example/servicestest1/mediaplayer/MediaPlayerActivity.kt
+++ app/src/main/java/com/example/servicestest1/mediaplayer/MediaPlayerActivity.kt
@@ -1,7 +1,8 @@
-package com.example.servicestest1
+package com.example.servicestest1.mediaplayer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.servicestest1.R
 
 class MediaPlayerActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
